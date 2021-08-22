5e10f67d75586ac9e2fa193d5e6703e1e4fa9d5c
Jan Bryński <brynski.jan@gmail.com>
2021-08-22 21:04:25 +0200

    svg image, post component dimensions

--- components/Post.tsx
+++ components/Post.tsx
@@ -4,6 +4,7 @@ import "@fontsource/montserrat";
 
 export default function Post() {
   return (
+    //frame
     <Box
       w={"343px"}
       h={"360px"}
@@ -11,8 +12,85 @@ export default function Post() {
       top={"0px"}
       border={"2px solid #000000"}
     >
-      {/* <Box position="relative" top="0px" w="339px" h="222px" bg="red"></Box> */}
+      <Box pos="relative" w="100%" h="65%">
+        <Image
+          alt="Rihanna"
+          src="/rihanna.svg"
+          layout="fill"
+          objectFit="cover"
+        />
+      </Box>
+      <Box
+        position={"relative"}
+        w={"339px"}
+        h={"2px"}
+        left={"0px"}
+        // top={"226px"}
+        bg={"#000000"}
+      />
+
+      {/* spacer */}
+
+      <Flex
+        position={"relative"}
+        alignItems={"center"}
+        p={"4px 8px"}
+        w={"73px"}
+        h={"22px"}
+        left={"10px"}
+        top={"-13px"}
+        bg={"#1300E8"}
+        border={"2px solid #000000"}
+        boxSizing={"border-box"}
+        boxShadow={"2px 2px 0px #000000"}
+      >
+        <Text
+          // fontFamily={"Montserrat"}
+          fontStyle={"normal"}
+          fontWeight={"900"}
+          fontSize={"11px"}
+          // lineHeight={"14px"}
+          color={"#FFFFFF"}
+        >
+          HIP-HOP
+        </Text>
+      </Flex>
+      <Box w="100%" h="40%">
+        <Text
+          position="relative"
+          width="319px"
+          height="60px"
+          top="-15px"
+          left="10px"
+          fontWeight={900}
+          fontSize="16px"
+          letterSpacing="-0.04em"
+          lineHeight="20px"
+        >
+          {"Poprzednia dekada nalezala do Drake`a. "}
+          {"Jak Kanadyjczyk wplynal na muzyke popularna?"}
+        </Text>
+        <Box
+          position="relative"
+          w="148px"
+          height="16px"
+          top="-20px"
+          left="10px"
+        >
+          <Text
+            fontSize="12px"
+            color="#1300E8"
+            letterSpacing="-0.04em"
+            fontWeight="800"
+            lineHeight="16px"
+          >
+            Piotr Żelazny
+          </Text>
+        </Box>
+      </Box>
+
       <Box position="absolute" w="48px" h="48px" right="0px" top="0px">
+        {/* ribbon square*/}
         <Box
           pos="absolute"
           left="0px"
@@ -43,63 +121,6 @@ export default function Post() {
             />
           </Box>
         </Box>
-      </Box>
-      <Box
-        position={"relative"}
-        w={"339px"}
-        h={"2px"}
-        left={"0px"}
-        top={"226px"}
-        bg={"#000000"}
-      />
-      <Flex
-        position={"relative"}
-        alignItems={"center"}
-        p={"4px 8px"}
-        w={"73px"}
-        h={"22px"}
-        left={"10px"}
-        top={"214px"}
-        bg={"#1300E8"}
-        border={"2px solid #000000"}
-        boxSizing={"border-box"}
-        boxShadow={"2px 2px 0px #000000"}
-      >
-        <Text
-          // fontFamily={"Montserrat"}
-          fontStyle={"normal"}
-          fontWeight={"900"}
-          fontSize={"11px"}
-          // lineHeight={"14px"}
-          color={"#FFFFFF"}
-        >
-          HIP-HOP
-        </Text>
-      </Flex>
-      <Text
-        position="relative"
-        width="319px"
-        height="60px"
-        top="210px"
-        left="10px"
-        fontWeight={900}
-        fontSize="16px"
-        letterSpacing="-0.04em"
-        lineHeight="20px"
-      >
-        {"Poprzednia dekada nalezala do Drake`a. "}
-        {"Jak Kanadyjczyk wplynal na muzyke popularna?"}
-      </Text>
-      <Box position="relative" w="148px" height="16px" top="206px" left="10px">
-        <Text
-          fontSize="12px"
-          color="#1300E8"
-          letterSpacing="-0.04em"
-          fontWeight="800"
-          lineHeight="16px"
-        >
-          Piotr Żelazny
-        </Text>
       </Box>
     </Box>
   );
